9628e979bb903f53dd4cd8daac75e25995326f37
Paul <pauljoshyk96@gmail.com>
2016-10-02 00:09:08 +0530

    made css a bit prettier

--- js/tShirtCtrl.js
+++ js/tShirtCtrl.js
@@ -3,12 +3,14 @@ tShirtApp.controller('tShirtCtrl',['$scope',function($scope){
 		{name:"T-shirt1",location:"/images/1.jpg",description:"lorem ipsum",price:""},
 		{name:"T-shirt2",location:"/images/2.jpg",description:"lorem ipsum",price:""},
 		{name:"T-shirt3",location:"/images/3.jpg",description:"lorem ipsum",price:""},
-		{name:"T-shirt4",location:"/images/1.jpg",description:"lorem ipsum",price:""},
-		{name:"T-shirt5",location:"/images/2.jpg",description:"lorem ipsum",price:""},
-		{name:"T-shirt1",location:"/images/3.jpg",description:"lorem ipsum",price:""},
-		{name:"T-shirt2",location:"/images/1.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt1",location:"/images/1.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt2",location:"/images/2.jpg",description:"lorem ipsum",price:""},
 		{name:"T-shirt3",location:"/images/3.jpg",description:"lorem ipsum",price:""},
-		{name:"T-shirt4",location:"/images/2.jpg",description:"lorem ipsum",price:""},
-		{name:"T-shirt5",location:"/images/1.jpg",description:"lorem ipsum",price:""}
+		{name:"T-shirt1",location:"/images/1.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt2",location:"/images/2.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt3",location:"/images/3.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt1",location:"/images/1.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt2",location:"/images/2.jpg",description:"lorem ipsum",price:""},
+		{name:"T-shirt3",location:"/images/3.jpg",description:"lorem ipsum",price:""}
 	];
 }])
